f359c306f0c7de3174671f089192b7031216ce57
Pawel Lorek <palorek@gmail.com>
2017-08-16 15:55:23 +0200

    Resize images before transforming to GIF
    it might not be in this exact step, but still - it's showing how we'll
    do it.
    Also things that may changes - I've put into constants

--- jpgs_to_gif.py
+++ jpgs_to_gif.py
@@ -1,7 +1,14 @@
 from PIL import Image
 
-im = Image.open('1.jpg')
-im2 = Image.open('2.jpg')
+IMAGE_SIZE=(430, 430)
 # duration - duration of each frame of the multiframe gif, in milliseconds. Pass a single integer for a constant duration, or a list or tuple to set the duration for each frame separately.
 # http://pillow.readthedocs.io/en/latest/handbook/image-file-formats.html?highlight=gif#saving
-im.save('out.gif', save_all=True, append_images=[im2], loop=0, duration=500)
+FRAME_DURATION=500 # ms
+
+im = Image.open('1.jpg')
+im2 = Image.open('2.jpg')
+
+out = im.resize(IMAGE_SIZE)
+out2 = im2.resize(IMAGE_SIZE)
+
+out.save('out.gif', save_all=True, append_images=[out2], loop=0, duration=500)
