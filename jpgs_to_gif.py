84b6c2ee3fd5433ec041dfbb66325abb1722dde5
Pawel Lorek <palorek@gmail.com>
2017-08-17 15:44:54 +0200

    Add color overlay, and bottom spacing for the white overlay

--- jpgs_to_gif.py
+++ jpgs_to_gif.py
@@ -6,17 +6,32 @@ OVERLAY_SIZE = (430, 90)
 # http://pillow.readthedocs.io/en/latest/handbook/image-file-formats.html?highlight=gif#saving
 FRAME_DURATION = 500 # ms
 
+#d6922d, #d86c27, #1b84c0
+FRAME_COLORS = [(214, 146, 45), (216, 108, 39), (27, 132, 192)]
+COLOR_OVERLAY_ALPHA = 0.4
+BOTTOM_SPACING = 20
+
+
 overlay = Image.open('group.png')
 overlay = overlay.resize(OVERLAY_SIZE) # SVG original is 600x126, so after scaling 126 * 430 / 600 = 90
 
-
-def prepare_frame(path, overlay):
+def read_img(path):
   img = Image.open(path)
-  out = img.resize(IMAGE_SIZE)
-  out.paste(overlay, box=(0, out.height - OVERLAY_SIZE[1]), mask=overlay)
-  return out
+  return img.resize(IMAGE_SIZE)
 
-frame1 = prepare_frame('1.jpg', overlay)
-frame2 = prepare_frame('2.jpg', overlay)
+def add_overlay(img, overlay):
+  img.paste(overlay, box=(0, img.height - OVERLAY_SIZE[1] - BOTTOM_SPACING), mask=overlay)
+  return img
 
-frame1.save('out.gif', save_all=True, append_images=[frame2], loop=0, duration=500)
+def blend_img(img, color):
+  color_img = Image.new('RGB', IMAGE_SIZE, color=color)
+  return Image.blend(img, color_img, COLOR_OVERLAY_ALPHA)
+
+
+frame1 = add_overlay(blend_img(read_img('1.jpg'), FRAME_COLORS[0]), overlay)
+frame2 = add_overlay(blend_img(read_img('2.jpg'), FRAME_COLORS[1]), overlay)
+frame3 = add_overlay(blend_img(read_img('3.jpg'), FRAME_COLORS[2]), overlay)
+
+frame1.save('out.gif', save_all=True, append_images=[frame2, frame3], loop=0, duration=500)
+
+
